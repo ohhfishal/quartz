451fd3b1d67fbcbaf364ce527fa800dda7fadc4d
ohhfishal <ohhfishal@gmail.com>
2025-09-22 21:54:41 -0700

    fix: page header to have less content

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "ohhfishal.net",
+    pageTitle: "ohhfishal",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,7 +34,7 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
+        // { Component: Component.ReaderMode() },
       ],
     }),
     Component.Explorer(),
